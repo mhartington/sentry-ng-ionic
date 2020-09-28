85611cbe6452354b88765b7381b2fe8d5e903c9c
Mike Hartington <mikehartington@gmail.com>
2020-09-28 19:55:06 -0400

    dont mod

--- src/main.ts
+++ src/main.ts
@@ -14,22 +14,22 @@ function normalizeUrl(url: string, pathStripRe: RegExp): string {
   return `app://${url.replace(/^file\:\/\//, '').replace(pathStripRe, '')}`;
 }
 
-Sentry.addGlobalEventProcessor((data:any) => {
-if (data.culprit) {
-    data.culprit = normalizeUrl(data.culprit, PATH_STRIP_RE);
-  }
-  const stacktrace =
-    data.stacktrace || (data.exception && data.exception.values && data.exception.values[0].stacktrace);
-  if (stacktrace) {
-    stacktrace.frames.forEach((frame: any) => {
-      if (frame.filename !== '[native code]' && frame.filename !== '<anonymous>') {
-        frame.filename = normalizeUrl(frame.filename, PATH_STRIP_RE);
-      }
-    });
-  }
-  console.log(data)
-  return data;
-})
+// Sentry.addGlobalEventProcessor((data:any) => {
+// if (data.culprit) {
+//     data.culprit = normalizeUrl(data.culprit, PATH_STRIP_RE);
+//   }
+//   const stacktrace =
+//     data.stacktrace || (data.exception && data.exception.values && data.exception.values[0].stacktrace);
+//   if (stacktrace) {
+//     stacktrace.frames.forEach((frame: any) => {
+//       if (frame.filename !== '[native code]' && frame.filename !== '<anonymous>') {
+//         frame.filename = normalizeUrl(frame.filename, PATH_STRIP_RE);
+//       }
+//     });
+//   }
+//   console.log(data)
+//   return data;
+// })
 if (environment.production) {
   enableProdMode();
 }
